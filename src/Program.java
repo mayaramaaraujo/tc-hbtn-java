a2ed37d7312bf4fbb4d878691c170be4df313c7d
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-01-26 17:15:26 -0300

    numero

--- src/Program.java
+++ src/Program.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Program {
+public class Program {
+    public static void main(String[] args) {
+        System.out.printf("%d\n", Numero.somar(24, 32));
+        System.out.printf("%d\n", Numero.somar(5, 0));
+        System.out.printf("%d\n", Numero.somar(4, -2));
+    }
 }
