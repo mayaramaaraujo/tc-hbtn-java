bac761ef87c9212dc67e3607fa0f4710888e9561
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-01-26 17:08:13 -0300

    teste

---
+++ 0x01/maiusculo/Program.java
@@ -0,0 +1,6 @@
+public class Program {
+    public static void main(String[] args) {
+        Caractere caractere = new Caractere();
+        System.out.println(caractere.EhMaiusculo('C'));
+    }
+}
---
+++ src/Caractere.java
@@ -0,0 +1,5 @@
+public class Caractere {
+    public static boolean EhMaiusculo(char c) {
+        return Character.isUpperCase(c);
+    }
+}
