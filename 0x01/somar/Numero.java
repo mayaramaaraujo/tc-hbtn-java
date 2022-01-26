4b10ae23c9924a57e2141457cfddca2683c3962d
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-01-26 17:13:50 -0300

    numero

---
+++ 0x01/somar/Numero.java
@@ -0,0 +1,5 @@
+public class Numero {
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+}
--- src/Caractere.java
+++
@@ -1,5 +0,0 @@
-public class Caractere {
-    public static boolean EhMaiusculo(char c) {
-        return Character.isUpperCase(c);
-    }
-}
