6c8ed73f535f2a8a75c4607424d072d534faaa45
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-01-27 10:17:44 -0300

    correcao

--- 0x03/encapsulamento/PersonagemGame.java
+++ 0x03/encapsulamento/PersonagemGame.java
@@ -1,6 +1,6 @@
 public class PersonagemGame {
-    public int saudeAtual;
-    public String nome;
+    private int saudeAtual;
+    private String nome;
 
     public int getSaudeAtual() {
         return saudeAtual;
