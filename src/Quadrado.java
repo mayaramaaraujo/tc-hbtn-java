8ea1f9ed37f1fdc1ded6de6d3dc80f36edff314e
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-01-27 10:12:55 -0300

    personagem

--- src/Quadrado.java
+++
@@ -1,9 +0,0 @@
-public class Quadrado {
-    public static double area(double lado) {
-       if (lado < 0) {
-           throw new IllegalArgumentException("Lado deve possuir valor positivo");
-       }
-
-       return lado * lado;
-    }
-}
