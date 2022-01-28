69ef842bcce95ba1c864fd0842463510284a81d6
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-01-28 14:45:20 -0300

    teste

--- 0x04/quadrado/Program.java
+++ 0x04/quadrado/Program.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 public class Program {
     public static void main(String[] args) {
         Quadrado quadrado = new Quadrado();
