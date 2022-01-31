f764854a2216800d27febf050fffe06e526bc3ea
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-01-31 11:40:22 -0300

    virgula

--- 0x05/salario/Gerente.java
+++ 0x05/salario/Gerente.java
@@ -6,7 +6,7 @@ public class Gerente extends Empregado {
     @Override
     public double calcularBonus(Departamento departamento) {
         if(departamento.alcancouMeta()) {
-            return (this.salarioFixo * 0.2);
+            return (this.salarioFixo * 0.2) + (departamento.getValorAtingidoMeta() - departamento.getValorMeta()) * 0.01;
         } else {
             return 0;
         }
