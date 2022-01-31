b964b2f67ebb882c3642a4865a2667c0006a10ba
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-01-31 14:37:07 -0300

    livraria

--- 0x05/livraria/LivroEdicaoOuro.java
+++ 0x05/livraria/LivroEdicaoOuro.java
@@ -1,6 +1,9 @@
+import exceptions.AutorInvalidoException;
+import exceptions.LivroInvalidoException;
+
 public class LivroEdicaoOuro extends Livro {
 
-    public LivroEdicaoOuro(String titulo, String autor, double preco) {
+    public LivroEdicaoOuro(String titulo, String autor, double preco) throws AutorInvalidoException, LivroInvalidoException {
         super(titulo, autor, preco);
     }
 
