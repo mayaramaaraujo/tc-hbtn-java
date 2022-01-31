caa692454a73d4ca83c856d25b4e9c3fe02388e1
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-01-31 10:32:17 -0300

    virgula

--- src/Program.java
+++ src/Program.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Modifier;
+import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
@@ -13,32 +14,32 @@ public class Program {
         Empregado empregadoDocumentacao2 = new Empregado(3000);
         Gerente gerenteDocumentacao = new Gerente(4980);
 
-        System.out.printf("Salario Fixo: %.2f - Salario Total: %.2f - Valor Bonus: %.2f\n",
+        System.out.printf(Locale.US,"Salario Fixo: %.2f - Salario Total: %.2f - Valor Bonus: %.2f\n",
                 empregadoEngenharia1.getSalarioFixo(),
                 empregadoEngenharia1.calcularSalarioTotal(departamentoEngenharia),
                 empregadoEngenharia1.calcularBonus(departamentoEngenharia));
 
-        System.out.printf("Salario Fixo: %.2f - Salario Total: %.2f - Valor Bonus: %.2f\n",
+        System.out.printf(Locale.US,"Salario Fixo: %.2f - Salario Total: %.2f - Valor Bonus: %.2f\n",
                 empregadoEngenharia2.getSalarioFixo(),
                 empregadoEngenharia2.calcularSalarioTotal(departamentoEngenharia),
                 empregadoEngenharia2.calcularBonus(departamentoEngenharia));
 
-        System.out.printf("Salario Fixo: %.2f - Salario Total: %.2f - Valor Bonus: %.2f\n",
+        System.out.printf(Locale.US,"Salario Fixo: %.2f - Salario Total: %.2f - Valor Bonus: %.2f\n",
                 gerenteEngenharia.getSalarioFixo(),
                 gerenteEngenharia.calcularSalarioTotal(departamentoEngenharia),
                 gerenteEngenharia.calcularBonus(departamentoEngenharia));
 
-        System.out.printf("Salario Fixo: %.2f - Salario Total: %.2f - Valor Bonus: %.2f\n",
+        System.out.printf(Locale.US,"Salario Fixo: %.2f - Salario Total: %.2f - Valor Bonus: %.2f\n",
                 empregadoDocumentacao1.getSalarioFixo(),
                 empregadoDocumentacao1.calcularSalarioTotal(departamentoDocumentacao),
                 empregadoDocumentacao1.calcularBonus(departamentoDocumentacao));
 
-        System.out.printf("Salario Fixo: %.2f - Salario Total: %.2f - Valor Bonus: %.2f\n",
+        System.out.printf(Locale.US,"Salario Fixo: %.2f - Salario Total: %.2f - Valor Bonus: %.2f\n",
                 empregadoDocumentacao2.getSalarioFixo(),
                 empregadoDocumentacao2.calcularSalarioTotal(departamentoDocumentacao),
                 empregadoDocumentacao2.calcularBonus(departamentoDocumentacao));
 
-        System.out.printf("Salario Fixo: %.2f - Salario Total: %.2f - Valor Bonus: %.2f\n",
+        System.out.printf(Locale.US,"Salario Fixo: %.2f - Salario Total: %.2f - Valor Bonus: %.2f\n",
                 gerenteDocumentacao.getSalarioFixo(),
                 gerenteDocumentacao.calcularSalarioTotal(departamentoDocumentacao),
                 gerenteDocumentacao.calcularBonus(departamentoDocumentacao));
