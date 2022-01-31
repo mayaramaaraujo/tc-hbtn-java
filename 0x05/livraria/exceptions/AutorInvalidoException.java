7464d0816ae6bde2c794f61f1a033ba7e04dca75
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-01-31 14:02:59 -0300

    livraria

--- 0x05/livraria/exceptions/AutorInvalidoException.java
+++
@@ -1,5 +0,0 @@
-public class AutorInvalidoException extends Exception {
-    public AutorInvalidoException(String message) {
-        super(message);
-    }
-}
--- 0x05/livraria/exceptions/LivroInvalidoException.java
+++
@@ -1,5 +0,0 @@
-public class LivroInvalidoException extends Exception {
-    public LivroInvalidoException(String message) {
-        super(message);
-    }
-}
