a81e37d4a43810d0ebdeb240f67ec37a15b59396
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-02-08 17:31:19 -0300

    banco

--- 0x11/banco/Banco.java
+++ 0x11/banco/Banco.java
@@ -73,7 +73,7 @@ public class Banco {
 
             int transacaoIndex = 1;
             for (double transacao: c.getTransacoes()) {
-                System.out.printf(" [" + transacaoIndex + "] valor ");
+                System.out.printf("  [" + transacaoIndex + "] valor ");
                 System.out.printf("%.2f\n", transacao);
                 transacaoIndex++;
             }
