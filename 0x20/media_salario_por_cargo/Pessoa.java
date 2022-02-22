ac574c9ce1af3f9da2526898e038589cea674410
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-02-22 14:39:21 -0300

    media salario

---
+++ 0x20/media_salario_por_cargo/Pessoa.java
@@ -0,0 +1,42 @@
+import java.util.Locale;
+
+public class Pessoa {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.FRANCE,"%s %s %s %s %s %f", "[" + codigo + "]", nome, cargo, idade, "R$", salario);
+    }
+}
