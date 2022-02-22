eeab6b24594751af633f4dfe4ba14f9336d4629d
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-02-22 14:28:15 -0300

    qtde por cargo

---
+++ 0x20/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+
+public class ConsultaPessoas {
+    static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
+       Map<String, List<Pessoa>> pessoasPorCargo = pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo));
+       return pessoasPorCargo;
+    }
+
+    static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.mapping(Pessoa::getIdade, toList())));
+    }
+
+    static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.filtering(pessoa -> pessoa.getIdade() > 40, Collectors.toList())));
+    }
+
+    static TreeMap<String, Set<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+
+        TreeMap<String, Set<Pessoa>> pessoasPorCargoEmOrdemReversa = pessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo,
+                        () -> new TreeMap<String, Set<Pessoa>>(Comparator.reverseOrder()),
+                        toCollection(() -> new TreeSet<>(Comparator.comparing(Pessoa::getNome)))
+                ));
+
+        return pessoasPorCargoEmOrdemReversa;
+    }
+
+    static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+    }
+}
