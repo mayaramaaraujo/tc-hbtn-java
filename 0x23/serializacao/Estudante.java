5a8d1dd87687661b374db7844dfdfa6bfa718360
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-02-28 09:42:36 -0300

    serializacao

--- 0x23/serializacao/Estudante.java
+++ 0x23/serializacao/Estudante.java
@@ -27,6 +27,6 @@ public class Estudante implements Serializable {
 
     @Override
     public String toString() {
-        return "Estudante { nome='" + nome + "', idade='" + idade + ", senha='" + senha + "' }";
+        return "Estudante { nome='" + nome + "', idade='" + idade + "', senha='" + senha + "' }";
     }
 }
