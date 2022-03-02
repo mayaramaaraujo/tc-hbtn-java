087a34bc96fd3a65c601d905079610d2fac13ff8
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-03-02 16:40:54 -0300

    jar 2

--- src/script.sh
+++ src/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-javac -d com/holbertonschool/main -cp lib/Matematica.jar com/holbertonschool/main/Program.java
+javac -cp lib/Matematica.jar com/holbertonschool/main/Program.java
 java -cp lib/Matematica.jar:. com.holbertonschool.main.Program
