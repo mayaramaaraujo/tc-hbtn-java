e7058d4dfcce8b30365df23cf1224aa013dd8112
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-03-03 09:33:22 -0300

    jar

---
+++ 0x24/jar_executavel/com/holbertonschool/matematica/Aritmeticos.java
@@ -0,0 +1,19 @@
+package com.holbertonschool.matematica;
+
+public class Aritmeticos {
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static int dividir(int a, int b) {
+        return a / b;
+    }
+}
